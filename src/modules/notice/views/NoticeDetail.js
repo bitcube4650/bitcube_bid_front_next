210b7568689c3a469b826e4fe68ad180825c5093
JAR <areum@bitcube.co.kr>
2024-05-27 08:35:23 +0900

    공지사항 등록

--- src/modules/notice/views/NoticeDetail.js
+++ src/modules/notice/views/NoticeDetail.js
@@ -1,12 +1,77 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 const NoticeDetail = () => {
     const { bno } = useParams();
+    const dataFromList = {};
+    const val = "";
+    const index = "";
+    const groupList = "";
 
     return (
         <div className="conRight">
-            NoticeDetail {bno}
+            <div className="conHeader">
+                <ul className="conHeaderCate">
+                    <li>공지사항</li>
+                    <li>공지사항 상세</li>
+                </ul>
+            </div>
+            <div className="contents">
+                <h3 className="h3Tit">공지사항</h3>
+                <div className="boxSt mt20">
+                    <div className="flex align-items-center">
+                        <div className="formTit flex-shrink0 width170px">제목</div>
+                        <div className="width100">{ dataFromList.btitle }</div>
+                    </div>
+                    <div className="flex align-items-center mt20">
+                        <div className="formTit flex-shrink0 width170px">공지대상</div>
+                        <div className="flex width100">
+                            <input type="radio" name="bm2" v-model="dataFromList.bco" value="ALL" id="bm2_1" className="radioStyle" disabled="dataFromList.bco == 'ALL' ? false : true" />
+                            <label for="bm2_1" className="dataFromList.bco == 'ALL' ? '' : 'dimmed'">공통</label>
+                            <div>
+                                <input type="radio" name= "bm2" v-model="dataFromList.bco" value="CUST" id="bm2_2" className="radioStyle" disabled="dataFromList.bco == 'CUST' ? false : true" />
+                                <label for="bm2_2" className="dataFromList.bco == 'CUST' ? '' : 'dimmed'">계열사</label>
+                                <p className="mt5 ml30" v-if="dataFromList.bco == 'CUST'">
+                                    <span v-for="(val, index) in groupList" key="index">
+                                        {/* val.interrelated.interrelatedNm }{ index < groupList.length - 1 ? ', ' : '' */}
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex align-items-center mt20">
+                        <div className="formTit flex-shrink0 width170px">작성자</div>
+                        <div className="width100">{ dataFromList.buserName }</div>
+                    </div>
+                    <div className="flex align-items-center mt20">
+                        <div className="formTit flex-shrink0 width170px">공지일시</div>
+                        <div className="width100">{ dataFromList.bdate }</div>
+                    </div>
+                    <div className="flex align-items-center mt20">
+                        <div className="formTit flex-shrink0 width170px">조회수</div>
+                        <div className="width100">{ dataFromList.bcount }</div>
+                    </div>
+                    <div className="flex align-items-center mt20">
+                        <div className="formTit flex-shrink0 width170px">첨부파일</div>
+                        <div className="width100" v-if="dataFromList.bfilePath != null && dataFromList.bfilePath != ''">
+                            <a click="downloadFile" id="file-download" className="textUnderline">{ dataFromList.bfile }</a>
+                        </div>
+                    </div>
+                    <div className="flex mt20">
+                        <div className="formTit flex-shrink0 width170px">공지내용</div>
+                        <div className="width100">
+                            <pre v-html="content" className="overflow-y-auto notiBox width100" style={{height:'400px', backgroundColor: 'white'}}>
+                            </pre>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="text-center mt50">
+                    <Link to="/notice" className="btnStyle btnOutline" title="목록">목록</Link>
+                    <a v-if="(custType == 'inter' && userAuth == '1') || (dataFromList.buserid == userId)" click="clickUpdate" className="btnStyle btnOutline" title="수정 이동">수정 이동</a>
+                    <a v-if="(custType == 'inter' && userAuth == '1') || (dataFromList.buserid == userId)" data-toggle="modal" data-target="#notiDel" className="btnStyle btnOutlineRed" title="삭제">삭제</a>
+                </div>
+            </div>
         </div>
     );
 };
--- src/modules/notice/views/NoticeEdit.js
+++ src/modules/notice/views/NoticeEdit.js
@@ -1,11 +1,23 @@
 import React, { useState } from 'react';
-import { useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
+import axios from 'axios';
+import Swal from 'sweetalert2'; // 공통 팝업창
 
 const NoticeEdit = () => {
+    //세션 로그인 정보
+    const loginInfo = JSON.parse(sessionStorage.getItem("loginInfo"));
+
+    const navigate = useNavigate();
     const location = useLocation();
+    const updateInsert = location.state.updateInsert;
 
     const [detailData, setDetailData] = useState({
-        btitle: "test"
+        btitle      : "test",
+        bco         : "ALL",
+        buserName   : loginInfo.userName,
+        buserid     : loginInfo.userId,
+        bcount      : 0,
+        bcontent    : "어쩌고저쩌고"
     });
     const onChangeDetailData = (e) => {
         setDetailData({
@@ -13,86 +25,134 @@ const NoticeEdit = () => {
             [e.target.name]: e.target.value
         });
     }
+    const selectedFile = null;
 
-    const updateInsert = location.state.updateInsert;
-    const val = {interrelated:{}};
-    const index = '';
-    const groupList = [];
-    const selectedFileName = '';
+    //저장
+    function onSaveNotice ( prams){
+        Swal.fire({
+            title: '공지사항 등록',          // 타이틀
+            text: '공지일시는 현재 처리되는 기준으로 저장됩니다. 저장 하시겠습니까?',  // 내용
+            icon: 'warning',                // success / error / warning / info / question
+            confirmButtonColor: '#3085d6',  // 기본옵션
+            confirmButtonText: '확인',      // 기본옵션
+            showCancelButton: true,         // conrifm 으로 하고싶을떄
+            cancelButtonColor: '#d33',      // conrifm 에 나오는 닫기버튼 옵션
+            cancelButtonText: '닫기'        // conrifm 에 나오는 닫기버튼 옵션
+        }).then(result => {
+            // 만약 Promise리턴을 받으면,
+            if (result.isConfirmed) { // 만약 모달창에서 confirm 버튼을 눌렀다면
+                if(updateInsert == "update") {
+                    //todo: 
+                } else {
+                    onInsertNotice();
+                }
+            }
+         });
+    };
 
+    async function onInsertNotice() {
+        try {
+            var formData = new FormData();
+            formData.append('file', selectedFile);
+            formData.append('data', JSON.stringify(detailData));
+
+            const response = await axios.post('/api/v1/notice/insertNotice', formData);
+            console.log(response);
+
+            if(response.status == 200) {
+                Swal.fire('등록되었습니다.', '', 'success');
+                navigate("/notice");
+            } else {
+                Swal.fire(response.message, '', 'error');
+            }
+        } catch (error) {
+            Swal.fire('등록에 실패하였습니다.', '', 'error');
+            console.log(error);
+        }
+    }
+    
     return (
         <div className="conRight">
-            <div class="conHeader">
-                <ul class="conHeaderCate">
+            <div className="conHeader">
+                <ul className="conHeaderCate">
                     <li>공지사항</li>
                     <li>공지사항 등록/수정</li>
                 </ul>
             </div>
-            <div class="contents">
-                <h3 class="h3Tit">{ updateInsert == 'update' ? '공지사항 수정' : '공지사항 등록' }</h3>
-                <div class="boxSt mt20">
-                    <div class="flex align-items-center">
-                        <div class="formTit flex-shrink0 width170px">제목</div>
-                        <div class="width100">
-                            <input type="text" class="inputStyle" placeholder="" onChange={ onChangeDetailData } value={detailData.btitle} name="btitle" maxlength="300" />
+            <div className="contents">
+                <h3 className="h3Tit">{ updateInsert == 'update' ? '공지사항 수정' : '공지사항 등록' }</h3>
+                <div className="boxSt mt20">
+                    <div className="flex align-items-center">
+                        <div className="formTit flex-shrink0 width170px">제목</div>
+                        <div className="width100">
+                            <input type="text" className="inputStyle" placeholder="" onChange={ onChangeDetailData } value={detailData.btitle} name="btitle" maxLength="300" />
                         </div>
                     </div>
-                    <div class="flex align-items-center mt20">
-                        <div class="formTit flex-shrink0 width170px">공지대상</div>
-                        <div class="flex width100">
-                            <input type="radio" name="bm2"  v-model="detailData.bco" value="ALL" id="bm2_1" class="radioStyle" disabled="userAuth != '1' ? true : false" />
+                    <div className="flex align-items-center mt20">
+                        <div className="formTit flex-shrink0 width170px">공지대상</div>
+                        <div className="flex width100">
+                            <input type="radio" id="bm2_1" className="radioStyle" onChange={ onChangeDetailData } name="bm2" value="ALL" checked={ detailData.bco == "ALL" } disabled={loginInfo.userAuth != '1' ? true : false} />
                             <label for="bm2_1">공통</label>
+                            todo: 계열사
+                            {/*
                             <div click="openModal">
-                                <input type="radio" name="bm2"  v-model="detailData.bco" value="CUST" id="bm2_2" class="radioStyle" />
+                                <input type="radio" name="bm2"  v-model="detailData.bco" value="CUST" id="bm2_2" className="radioStyle" />
                                     <label for="bm2_2" data-toggle="modal">계열사</label>
-                                <p class="mt5 ml30" v-if="detailData.bco == 'CUST'">
+                                <p className="mt5 ml30" v-if="detailData.bco == 'CUST'">
                                     <span v-for="(val, index) in groupList" key="index">
                                         { val.interrelated.interrelatedNm }{ index < groupList.length - 1 ? ', ' : '' }
                                     </span>
                                 </p>
                             </div>
+                            */}
                         </div>
                     </div>
-                    <div class="flex align-items-center mt20">
-                        <div class="formTit flex-shrink0 width170px">작성자</div>
-                        <div class="width100">{ detailData.buserName }</div>
+                    <div className="flex align-items-center mt20">
+                        <div className="formTit flex-shrink0 width170px">작성자</div>
+                        <div className="width100">{ detailData.buserName }</div>
                     </div>
-                    <div class="flex align-items-center mt20">
-                        <div class="formTit flex-shrink0 width170px">공지일시</div>
-                        <div class="width100">등록 또는 수정한 날짜로 저장됩니다.</div>
+                    <div className="flex align-items-center mt20">
+                        <div className="formTit flex-shrink0 width170px">공지일시</div>
+                        <div className="width100">등록 또는 수정한 날짜로 저장됩니다.</div>
                     </div>
-                    <div class="flex align-items-center mt20">
-                        <div class="formTit flex-shrink0 width170px">조회수</div>
-                        <div class="width100">{ detailData.bcount }</div>
+                    <div className="flex align-items-center mt20">
+                        <div className="formTit flex-shrink0 width170px">조회수</div>
+                        <div className="width100">{ detailData.bcount }</div>
                     </div>
-                    {/*
-                    <div class="flex mt20">
-                        <div class="formTit flex-shrink0 width170px">첨부파일</div>
-                        <div class="width100">
-                            <div class="upload-boxWrap">
-                                <div class="upload-box" v-show="!selectedFile">
+                    <div className="flex mt20">
+                        <div className="formTit flex-shrink0 width170px">첨부파일</div>
+                        <div className="width100">
+                            <div className="upload-boxWrap">
+                                todo: 첨부처리
+                                {/*
+                                <div className="upload-box" v-show="!selectedFile">
                                     <input type="file" ref="uploadedFile" id="file-input" change="changeFile" />
-                                    <div class="uploadTxt">
-                                        <i class="fa-regular fa-upload"></i>
+                                    <div className="uploadTxt">
+                                        <i className="fa-regular fa-upload"></i>
                                         <div>클릭 혹은 파일을 이곳에 드롭하세요.(암호화 해제)<br />파일 최대 10MB (등록 파일 개수 최대 1개)</div>
                                     </div>
                                 </div>
-                                <div class="uploadPreview" v-if="selectedFile">
+                                <div className="uploadPreview" v-if="selectedFile">
                                     <p>
                                         {{ selectedFileName }}
-                                        <button class='file-remove' click="fnRemoveAttachFile()">삭제</button>
+                                        <button className='file-remove' click="fnRemoveAttachFile()">삭제</button>
                                     </p>
                                 </div>
+                                */}
                             </div>
                         </div>
                     </div>
-                    */}
-                    <div class="flex mt20">
-                        <div class="formTit flex-shrink0 width170px">공지내용</div>
-                        <div class="width100">
-                            <textarea v-model="detailData.bcontent" class="textareaStyle notiBox overflow-y-auto" placeholder="" style={{height:'400px'}}>
+                    <div className="flex mt20">
+                        <div className="formTit flex-shrink0 width170px">공지내용</div>
+                        <div className="width100">
+                            <textarea className="textareaStyle notiBox overflow-y-auto" placeholder="" style={{height:'400px'}} onChange={ onChangeDetailData } name="bcontent" >
+                                { detailData.bcontent }
                             </textarea>
                         </div>
+                    </div>
+                    <div className="text-center mt50">
+                        <Link to="/notice" className="btnStyle btnOutline" title="목록">목록</Link>
+                        <a onClick={ onSaveNotice } className="btnStyle btnPrimary" title="저장">저장</a>
                     </div>
                 </div>
             </div>
