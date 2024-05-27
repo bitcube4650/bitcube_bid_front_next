9947a3925c223070b743d00b3dfb2db98d311759
JAR <areum@bitcube.co.kr>
2024-05-27 15:53:06 +0900

    공지 리스트 수정

--- src/modules/notice/components/NoticeList.js
+++ src/modules/notice/components/NoticeList.js
@@ -14,12 +14,12 @@ function NoticeList(props) {
             <td>{ props.notice.rowNo }</td>
             <td className="text-left">
                 <a onClick={ onNoticeDetail } className="textUnderline notiTitle" title="공지사항 자세히 보기">
-                    { props.notice.bco == 'ALL'?<span>[공통] </span>:"" }
+                    { props.notice.bco == 'ALL' && <span>[공통] </span> }
                     { props.notice.btitle }
                 </a>
             </td>
             <td>
-                { props.notice.bfile?<i className="fa-regular fa-file-lines notiFile"></i>:"" }
+                { props.notice.bfile && <i className="fa-regular fa-file-lines notiFile"></i> }
             </td>
             <td>{ props.notice.buserName }</td>
             <td>{ props.notice.bdate }</td>
