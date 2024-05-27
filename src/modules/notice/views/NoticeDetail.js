7b17b958aa26b9e315603c8271f324d36c282f99
JAR <areum@bitcube.co.kr>
2024-05-27 14:11:49 +0900

    공지사항 상세

--- src/modules/notice/views/NoticeDetail.js
+++ src/modules/notice/views/NoticeDetail.js
@@ -4,31 +4,40 @@ import axios from 'axios';
 import Swal from 'sweetalert2'; // 공통 팝업창
 
 const NoticeDetail = () => {
+    //세션 로그인 정보
+    const loginInfo = JSON.parse(sessionStorage.getItem("loginInfo"));
     const navigate = useNavigate();
     const { bno } = useParams();
     const [dataFromList, setDataFromList] = useState({});
+    const [groupList, setGroupList] = useState([]);
 
     async function onSelectDetail() {
         try {
             let srcData = {bno: bno};
             const response = await axios.post('/api/v1/notice/noticeList', srcData);
-
-            if(response.status == 200) {
+            //const groupResponse = await axios.post('/api/v1/notice/selectGroupList', srcData);
+            const groupResponse = {
+                status : 200,
+                data : ["1", "2", "3"]
+            }
+            
+            if(response.status == 200 && groupResponse.status == 200) {
                 setDataFromList(response.data.content[0]);
+                setGroupList(groupResponse.data);
             } else {
-                Swal.fire(response.message, '', 'error');
+                Swal.fire('조회에 실패하였습니다.', '', 'error');
                 navigate("/notice");
             }
         } catch (error) {
             Swal.fire('조회에 실패하였습니다.', '', 'error');
             console.log(error);
-            //navigate("/notice");
+            navigate("/notice");
         }
     };
 
     useEffect(() => {
         onSelectDetail();
-    });
+    }, bno);
 
     return (
         <div className="conRight">
@@ -54,9 +63,7 @@ const NoticeDetail = () => {
                                 <input type="radio" name= "bm2" value="CUST" id="bm2_2" className="radioStyle" checked={ dataFromList.bco == "CUST" } disabled={ dataFromList.bco != 'CUST' && 'true' } />
                                 <label for="bm2_2" className={ dataFromList.bco != 'CUST' && 'dimmed' }>계열사</label>
                                 <p className="mt5 ml30" v-if="dataFromList.bco == 'CUST'">
-                                    <span v-for="(val, index) in groupList" key="index">
-                                        {/* val.interrelated.interrelatedNm }{ index < groupList.length - 1 ? ', ' : '' */}
-                                    </span>
+                                    { groupList?.map((group, index) => (<span>{ group }{ index < groupList.length - 1 && ', ' }</span>)) }
                                 </p>
                             </div>
                         </div>
@@ -75,14 +82,18 @@ const NoticeDetail = () => {
                     </div>
                     <div className="flex align-items-center mt20">
                         <div className="formTit flex-shrink0 width170px">첨부파일</div>
-                        <div className="width100" v-if="dataFromList.bfilePath != null && dataFromList.bfilePath != ''">
-                            <a click="downloadFile" id="file-download" className="textUnderline">{ dataFromList.bfile }</a>
-                        </div>
+                        { dataFromList.bfilePath != null && dataFromList.bfilePath != '' &&
+                            <div className="width100">
+                                {/* todo: downloadFile */}
+                                <a click="downloadFile" id="file-download" className="textUnderline">{ dataFromList.bfile }</a>
+                            </div>
+                        }
                     </div>
                     <div className="flex mt20">
                         <div className="formTit flex-shrink0 width170px">공지내용</div>
                         <div className="width100">
-                            <pre v-html="content" className="overflow-y-auto notiBox width100" style={{height:'400px', backgroundColor: 'white'}}>
+                            <pre className="overflow-y-auto notiBox width100" style={{height:'400px', backgroundColor: 'white'}}>
+                                { dataFromList.bcontent }
                             </pre>
                         </div>
                     </div>
@@ -90,8 +101,15 @@ const NoticeDetail = () => {
 
                 <div className="text-center mt50">
                     <Link to="/notice" className="btnStyle btnOutline" title="목록">목록</Link>
-                    <a v-if="(custType == 'inter' && userAuth == '1') || (dataFromList.buserid == userId)" click="clickUpdate" className="btnStyle btnOutline" title="수정 이동">수정 이동</a>
-                    <a v-if="(custType == 'inter' && userAuth == '1') || (dataFromList.buserid == userId)" data-toggle="modal" data-target="#notiDel" className="btnStyle btnOutlineRed" title="삭제">삭제</a>
+                    {/* todo: 수정/삭제 */}
+                    {
+                        ((loginInfo.custType == 'inter' && loginInfo.userAuth == '1') || dataFromList.buserId == loginInfo.userId) &&
+                        <a click="clickUpdate" className="btnStyle btnOutline" title="수정 이동">수정 이동</a>
+                    }
+                    {
+                        ((loginInfo.custType == 'inter' && loginInfo.userAuth == '1') || dataFromList.buserId == loginInfo.userId) &&
+                        <a data-toggle="modal" data-target="#notiDel" className="btnStyle btnOutlineRed" title="삭제">삭제</a>
+                    }
                 </div>
             </div>
         </div>
