fd5ec2378fc9e7dd4aecb3260be300514871530f
bitcube <bitcube@DESKTOP-U72KE70>
2024-05-31 09:05:51 +0900

    로그인 변환작업

---
+++ src/components/login/LoginNotAppPop.js
@@ -0,0 +1,24 @@
+import React, { useCallback, useState } from 'react'
+import Modal from 'react-bootstrap/Modal';
+
+const LoginNotAppPop = ({setLoginNotAppPop, loginNotAppPop}) => {
+    
+    // 팝업 닫기
+    const fnCloseLoginNotAppPop = useCallback(() => {
+        setLoginNotAppPop(false);
+    })
+
+    return (
+        <Modal className="modalStyle" id="loginAlert" show={loginNotAppPop} onHide={fnCloseLoginNotAppPop} keyboard={true}>
+            <Modal.Body>
+                <a onClick={fnCloseLoginNotAppPop} className="ModalClose" data-bs-dismiss="modal" title="닫기"><i className="fa-solid fa-xmark"></i></a>
+                <div className="alertText1">입력하신 아이디와 비밀번호는 일치하나, 아직 본사의 승인이 이루어지지 않았습니다. 본사의 승인 후 본 전자입찰/계약 시스템을 이용하실 수 있습니다. 본사승인 후 다시 로그인 해 주세요.</div>
+                <div className="modalFooter">
+                    <a onClick={fnCloseLoginNotAppPop} className="modalBtnClose" data-dismiss="modal" title="닫기">닫기</a>
+                </div>
+            </Modal.Body>
+        </Modal>
+    )
+}
+
+export default LoginNotAppPop
--- src/components/login/LoginWrap.js
+++ src/components/login/LoginWrap.js
@@ -1,72 +1,145 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from "react-router-dom";
+import { useCookies } from 'react-cookie';
+import LoginFailPop from "../login/LoginFailPop";
+import LoginNotAppPop from "../login/LoginNotAppPop";
+import Swal from 'sweetalert2';
 import axios from "axios"
+
+
 
 //todo: 화면 대충 복붙해서 오류나는 부분 수정만 해서 다시 복붙해서 한줄씩 수정 필요...
 
 function LoginWrap(props) {
-    {/* input value 처리 */}
-    const [value, setValue] = useState({});
-    function onChangeInputValue(e) {
-        setValue({
-            ...value,
-            [e.target.name]: e.target.value
-        });
-    }
+    const [loginInfo, setLoginInfo] = useState({
+        loginId: '',
+        loginPw: '',
+        token: ''
+    });    
+
+    const [rememberMe, setRememberMe] = useState(false);
+    const [cookies, setCookie, removeCookie] = useCookies(['rememberUserId', 'loginInfo']);
+
+    // 로그인 실패 팝업
+    const [loginFailPop, setLoginFailPop] = useState(false);
+
+    // 로그인 승인 실패 팝업
+    const [loginNotAppPop, setLoginNotAppPop] = useState(false);
 
     const navigate = useNavigate();
 
-    async function onLogin() {
-        try {
-            const response = await axios.post('/login', {
-                loginId: value.username,
-                loginPw: value.password
-            });
-            console.log(response);
-
-            if(response.status === 200) {
-                sessionStorage.setItem("loginInfo", JSON.stringify(response.data));
-                navigate("/main");
-            } else {
-                alert(response.message);    //todo: modal 표시 처리........
-            }
-        } catch (error) {
-            alert("아이디 또는 비밀번호를 확인해 주십시오.");    //todo: modal 표시 처리........
-            console.log(error);
+    
+    useEffect(() => {
+        const userId = cookies.rememberUserId;
+        if (userId) {
+          setRememberMe(true);
+          setLoginInfo({ ...loginInfo, loginId: userId });
         }
+    }, [cookies.rememberUserId]);
+
+    const onLogin = async () => {
+        const id = loginInfo.loginId.trim();
+        if(id === "") {
+            Swal.fire('', '아아디를 입력해 주십시오', 'error');
+            return;
+        }
+
+        if (loginInfo.loginPw === "") {
+            Swal.fire('', '비밀번호를 입력해 주십시오', 'error');
+            return;
+        }
+
+        try {
+            const response = await axios.post('/login', loginInfo);
+            const loginData = response.data;
+            if(response.status === 200) {
+                setCookie('loginInfo', JSON.stringify(loginData));
+                sessionStorage.setItem("loginInfo", JSON.stringify(response.data));
+                if (rememberMe) {
+                    setCookie('rememberUserId', loginData.userId);
+                } else {
+                    removeCookie('rememberUserId');
+                }
+                axios.defaults.headers['x-auth-token'] = loginData.token;
+                navigate("/main");
+            }
+
+        } catch (err) {
+            // 기존 로그인 로직 interface처리하는부분의 로직이 사라져서 case처리 부분 삭제
+            if (err.response.status === 403) {
+                setLoginNotAppPop(true);
+            } else {
+                setLoginFailPop(true);
+            }
+        }
+    };
+
+    const onSignUp = () => {
+        navigate("/SignUp");
     }
-
+    
     return (
-        <div className="loginWrap">
-            <div className="loginLeft">
-                <h1><img src={props.logoUrl} className="img-responsive" alt="일진그룹 로고" /></h1>
+        <div class="loginWrap">
+            <div class="loginLeft">
+                <h1><img src={props.logoUrl} class="img-responsive" alt="일진그룹 로고" /></h1>
 
-                <input onChange={onChangeInputValue} autoFocus="" autoComplete="name" type="text" name="username" className="loginInputStyle" placeholder="아이디" />
-                <input onChange={onChangeInputValue} autoComplete="new-password" type="password" name="password" className="loginInputStyle mt10" placeholder="비밀번호" />
+                <input
+                    type="text"
+                    value={loginInfo.loginId}
+                    onChange={(e) => setLoginInfo({ ...loginInfo, loginId: e.target.value })}
+                    autoComplete="name"
+                    name="username"
+                    placeholder="아이디"
+                    autoFocus
+                    className="loginInputStyle"
+                />
+                <input
+                    type="password"
+                    value={loginInfo.loginPw}
+                    onChange={(e) => setLoginInfo({ ...loginInfo, loginPw: e.target.value })}
+                    autoComplete="new-password"
+                    name="password"
+                    className="loginInputStyle mt10"
+                    placeholder="비밀번호"
+                />
+
+                {/* <input onChange={onChangeInputValue} autofocus="" autocomplete="name" type="text" name="username" class="loginInputStyle" placeholder="아이디" />
+                <input onChange={onChangeInputValue} autocomplete="new-password" type="password" name="password" class="loginInputStyle mt10" placeholder="비밀번호" /> */}
 
                 <div className="loginFindWrap">
-                    <input type="checkbox" id="chkID" v-model="rememberMe" className="loginCheckStyle" /><label htmlFor="chkID">아이디 저장</label>
-                    <ul className="loginFind">
-                        <li><a href="/#" data-toggle="modal" data-target="#idSearch" title="아이디 찾기">아이디 찾기</a></li>
-                        <li><a href="/#" data-toggle="modal" data-target="#pwSearch" title="비밀번호 찾기">비밀번호 찾기</a></li>
+                    <input type="checkbox" id="chkID" checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} className="loginCheckStyle"/>
+                    <label htmlFor="chkID">아이디 저장</label>
+                    {/* <input type="checkbox" id="chkID" v-model="rememberMe" class="loginCheckStyle" /><label htmlFor="chkID">아이디 저장</label> */}
+                    <ul class="loginFind">
+                        <li><a href="#" data-toggle="modal" data-target="#idSearch" title="아이디 찾기">아이디 찾기</a></li>
+                        <li><a href="#" data-toggle="modal" data-target="#pwSearch" title="비밀번호 찾기">비밀번호 찾기</a></li>
                     </ul>
                 </div>
-                <div className="loginBtnWrap">
-                    <a href="/#" onClick={onLogin} className="btnLoginPrimary" title="로그인">로그인</a>
-                    <a href="/signup"  className="btnLoginOutline mt10" title="회원가입">회원가입</a>
+                <div class="loginBtnWrap">
+                    <a onClick={onLogin} class="btnLoginPrimary" title="로그인">로그인</a>
+                    <a onClick={onSignUp} class="btnLoginOutline mt10" title="회원가입">회원가입</a>
+                    {/* <router-link to="/signup"  class="btnLoginOutline mt10" title="회원가입">회원가입</router-link> */}
                 </div>
             </div>
-            <div className="loginRight">
+            <div class="loginRight">
                 <h2><span>투명</span>합니다.</h2>
                 <h2><span>함께</span>합니다.</h2>
                 <h2><span>미래</span>를 엽니다.</h2>
                 <h3>" CLEAR, UNITED, OPENING THE FUTURE "</h3>
-                <div className="loginRight">
+                <div class="loginRight">
                     <h3 style={{fontSize: '30px', color: '#F3B352', fontWeight: 550}}>IT HelpDesk</h3>
-                    <h3 style={{marginTop: '5px', fontSize: '30px', fontWeight: 550}}>Tel : 02-720-4650</h3>
+                    <h3 style={{marginTop: '5px', fontSize: '30px', fontWeight: 550}}>Tel : 080-707-9100</h3>
                 </div>
             </div>
+
+            {/* 로그인 실패 */}
+            <LoginFailPop loginFailPop={loginFailPop} setLoginFailPop={setLoginFailPop} />
+            {/* 로그인 실패 */}
+
+            {/* 로그인 인증 실패 */}
+            <LoginNotAppPop loginNotAppPop={loginNotAppPop} setLoginNotAppPop={setLoginNotAppPop} />
+            {/* 로그인 인증 실패 */}
         </div>
     )
 }
