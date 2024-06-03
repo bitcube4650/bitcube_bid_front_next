1832fbef4630ff0e4398139edd4936e5cbf45845
강민지 <lami34@bitcube.co.kr>
2024-06-03 16:42:27 +0900

    파일내용넣음

---
+++ src/modules/bid/views/PartnerBidComplete.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PartnerBidComplete = () => {
+    return (
+        <div>PartnerBidComplete</div>
+    )
+}
+
+export default PartnerBidComplete
---
+++ src/modules/bid/views/PartnerBidCompleteDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PartnerBidCompleteDetail = () => {
+    return (
+        <div>PartnerBidCompleteDetail</div>
+    )
+}
+
+export default PartnerBidCompleteDetail
