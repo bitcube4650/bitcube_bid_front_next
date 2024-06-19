79ae237aa0f804554a193808d154797a05630d36
이의진 <destiny09090@bitcube.co.kr>
2024-06-19 17:46:29 +0900

    주석추 가

--- src/components/input/SrcSelectBox.tsx
+++ src/components/input/SrcSelectBox.tsx
@@ -9,7 +9,11 @@ const SrcSelectBox = (props: SrcSelectBoxProps) => {
         });
     }
 
-
+    // 샘플
+    // const [useYnOptionList, setUseYnOptionList] = useState([{"value" : "Y", "name" : "사용"}, {"value" : "N", "name" : "미사용"}])
+    // valueKey="value" nameKey="name" 식으로 세팅, 만약 가져온 코드성 값 리스트가 value, name 이 아닌 경우 valueKey / nameKey 의 값을 수정할 것
+    // <SrcSelectBox   name={"useYn"} optionList={useYnOptionList} valueKey="value" nameKey="name" .... />
+    // <SrcSelectBox   name={"interrelatedCustCode"} optionList={InterrelatedCustCodeList} valueKey="interrelatedCustCode" nameKey="interrelatedNm" .... />
     
     return (
         <select name={props.name} onChange={onFormEventSrcData} className="selectStyle">
